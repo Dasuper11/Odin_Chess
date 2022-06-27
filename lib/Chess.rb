7f0db055f681b48b5e6369d0ecf12d22c6970aa6
Dasuper11 <wrjp11@gmail.com>
2022-06-27 11:54:22 -0700

    added knight and scan functionality

--- lib/Chess.rb
+++ lib/Chess.rb
@@ -19,13 +19,26 @@ class Chess
      [4, 3, 2, 5, 6, 2, 3, 4]]
   end
 
+  def display
+    piece_dictionary = {0 => '* ', -1 => "\u2659 ", -2 => "\u2657 ", -3 => "\u2658 ", -4 => "\u2656 ", -5 => "\u2655 ", -6 => "\u2654 ",
+                        1 => "\u265F ", 2 => "\u265D ", 3 => "\u265E ", 4 => "\u265C ", 5 => "\u265B ", 6 => "\u265A "}
+    @board.each do |row|
+      line = ''
+      row.each do |piece|
+        line << piece_dictionary[piece]
+      end
+      puts line
+    end
+  end
+
   # player enters column, then row
   def play(start, dest)
-    piece_id = @board[start[1]][start[0]]
-    return 'invalid move' if piece_id.zero? || legal?(piece_id, start, dest) == false
-
-    @board[dest[1]][dest[0]] = piece_id
-    @board[start[1]][start[0]] = 0
+    piece_id = read_board(start)
+    return dest if piece_id.zero? || legal?(piece_id, start, dest) == false
+    dest.reverse
+    write_board(dest, piece_id)
+    write_board(start, 0)
+    #promote(dest) if piece_id.abs == 1 && ()
     self.turn_switch
   end
 
@@ -33,10 +46,22 @@ class Chess
     @turn = @turn == 1 ? -1 : 1
   end
 
-  def legal?(piece_id, start, dest)
-    return false if dest.any? { |coord| coord.negative? || coord > 7 }
+  def read_board(pos)
+    return nil if pos.any? { |num| num > 7 || num.negative? }
 
-    piece_id = abs(piece_id)
+    @board[pos[0]][pos[1]]
+  end
+
+  def write_board(pos, val)
+    return 'error' if pos.any? { |num| num > 7 || num.negative? }
+
+    @board[pos[0]][pos[1]] = val
+  end
+
+  def legal?(piece_id, start, dest)
+    return false if dest.any? { |coord| coord.negative? || coord > 7 } || (piece_id * @turn).negative?
+
+    piece_id = piece_id.abs
     case piece_id
     when 1
       return pawn_moves(start).any? { |move| move == dest }
@@ -55,31 +80,42 @@ class Chess
 
   def pawn_moves(start)
     moves = []
-    row = start[1]
-    col = start[0]
+    row = start[0]
+    col = start[1]
     one_ahead = row - 1 * @turn
     two_ahead = row - 2 * @turn
     # two moves at start
-    moves.append([col, two_ahead]) if ((row == 6 && @turn == 1) || (row == 1 && @turn == -1)) &&
-                                      piece_in_way?([col, one_ahead]) == false &&
-                                      piece_in_way?([col, two_ahead]) == false
+    moves.append([two_ahead, col]) if ((row == 6 && @turn == 1) || (row == 1 && @turn == -1)) &&
+                                      piece_in_way?([one_ahead, col]) == false &&
+                                      piece_in_way?([two_ahead, col]) == false
     # move one space
-    moves.append([col, one_ahead]) if piece_in_way?([col, one_ahead]) == false
+    moves.append([one_ahead, col]) if piece_in_way?([one_ahead, col]) == false
     # attack left
-    moves.append([col-1,one_ahead]) if piece_in_way?([col-1, one_ahead]) &&
-                                       self.enemey?
+    moves.append([one_ahead, col-1]) if piece_in_way?([one_ahead, col-1]) &&
+                                       self.enemey?([one_ahead, col-1])
     # attack right
-    moves.append([col+1,one_ahead]) if piece_in_way?([col+1, one_ahead]) &&
-                                       self.enemey?
+    moves.append([one_ahead, col+1]) if piece_in_way?([one_ahead, col+1]) &&
+                                       self.enemey?([one_ahead, col+1])
     return moves
   end
 
   def bishop_moves(start)
+   moves = []
 
   end
 
   def knight_moves(start)
-
+    moves = []
+    row, col = start
+    moves.append([row - 1, col + 2]) if piece_in_way?([row - 1, col + 2]) == false || enemey?([row - 1, col + 2])
+    moves.append([row - 1, col - 2]) if piece_in_way?([row - 1, col - 2]) == false || enemey?([row - 1, col - 2])
+    moves.append([row + 1, col + 2]) if piece_in_way?([row + 1, col + 2]) == false || enemey?([row + 1, col + 2])
+    moves.append([row + 1, col - 2]) if piece_in_way?([row + 1, col - 2]) == false || enemey?([row + 1, col - 2])
+    moves.append([row - 2, col + 1]) if piece_in_way?([row - 2, col + 1]) == false || enemey?([row - 2, col + 1])
+    moves.append([row - 2, col - 1]) if piece_in_way?([row - 2, col - 1]) == false || enemey?([row - 2, col - 1])
+    moves.append([row + 2, col + 1]) if piece_in_way?([row + 2, col + 1]) == false || enemey?([row + 2, col + 1])
+    moves.append([row + 2, col - 1]) if piece_in_way?([row + 2, col - 1]) == false || enemey?([row + 2, col - 1])
+    moves
   end
 
   def rook_moves(start)
@@ -94,12 +130,27 @@ class Chess
 
   end
 
+  def scan_direction(start, row_d = -1, col_d = 0)
+    moves = []
+    scan = [start[0]+row_d, start[1]+col_d]
+    while piece_in_way?(scan) == false
+      moves.append(scan)
+      scan = [scan[0]+row_d, scan[1]+col_d]
+    end
+    moves.append(scan) if enemey?(scan)
+    moves
+  end
+
   def piece_in_way?(dest)
-    @board[dest[1]][dest[0]].nonzero? || dest.any? { |num| num > 7 || num.negative? }
+    return true if read_board(dest).nil?
+
+    read_board(dest).nonzero? || dest.any? { |num| num > 7 || num.negative? }
   end
 
   def enemey?(dest)
-    (@board[dest[1]][dest[0]] * @turn).negative?
+    return false if read_board(dest).nil?
+
+    (read_board(dest) * @turn).negative?
   end
 
   def promote
--- spec/Chess_spec.rb
+++ spec/Chess_spec.rb
@@ -1,1 +1,82 @@
+#./lib/Chess_spec
 require './lib/Chess'
+
+describe '#Chess' do
+  context 'when moving pawn' do
+    it 'can move forward one' do
+      game = Chess.new
+      game.play([6,3],[5,3])
+      expect(game.read_board([5,3])).to eql(1)
+    end
+
+    it 'can move forward two on from initial row' do
+      game = Chess.new
+      game.play([6,3],[4,3])
+      expect(game.read_board([4,3])).to eql(1)
+    end
+
+    it 'changes turn' do
+      game = Chess.new
+      game.play([6,3],[5,3])
+      expect(game.instance_variable_get('@turn')).to eql(-1)
+    end
+
+    it 'can take enemey piece diagonally' do
+      game = Chess.new
+      game.play([6,3],[4,3])
+      game.play([1,2],[3,2])
+      game.play([4,3],[3,2])
+      expect(game.read_board([3,2])).to eql(1)
+    end
+
+    it 'cannot move forward into another piece' do
+      game = Chess.new
+      game.play([6,3],[4,3])
+      game.play([1,3],[3,3])
+      game.play([4,3],[3,3])
+      expect(game.instance_variable_get('@turn')).to eql(1)
+    end
+
+    it 'cannot move two spaces after first move' do
+      game = Chess.new
+      game.play([6,3],[5,3])
+      game.play([1,2],[3,2])
+      game.play([5,3],[3,3])
+      expect(game.instance_variable_get('@turn')).to eql(1)
+    end
+
+    it 'cannot move another players piece' do
+      game = Chess.new
+      game.play([1,2],[3,2])
+      expect(game.instance_variable_get('@turn')).to eql(1)
+    end
+  end
+
+  context 'when moving knight' do
+    it 'moves in correct pattern' do
+      game = Chess.new
+      game.play([7,1],[5,0])
+      expect(game.read_board([5,0])).to eql(3)
+    end
+
+    it 'can attack enemey piece' do
+      game = Chess.new
+      game.play([7,1],[5,0])
+      game.play([1,1],[3,1])
+      game.play([5,0],[3,1])
+      expect(game.read_board([3,1])).to eql(3)
+    end
+    
+    it 'cannot move onto friendly piece' do
+      game = Chess.new
+      game.play([7,1],[6,3])
+      expect(game.instance_variable_get('@turn')).to eql(1)
+    end
+
+    it 'cannot move off board' do
+      game = Chess.new
+      game.play([7,1],[9,0])
+      expect(game.instance_variable_get('@turn')).to eql(1)
+    end
+  end
+end
